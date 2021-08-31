6b4dfe38bee83584f0ebca5f268808c4ad851206
MunLolYes <87713245+MunLolYes@users.noreply.github.com>
2021-08-31 21:29:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Hello, World.");
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.on("ready", () => {
+    console.log("Bot started!");
+});
+
+client.on('messageCreate', message => {
+    if (message.content === '$mom') {
+      message.channel.send('moms are epic');
+    }
+  });
+
+client.on('messageCreate', message => {
+  if (message.content === "$rank") {
+    message.channel.send("Nope, not that type of bot.");
+  }
+});
+
+client.login("ODgxODU4OTQyMjE5NzM1MDYw.YSy9Iw.98z2R8lJGzYZ0Y3xw-X3LZkejc0");
